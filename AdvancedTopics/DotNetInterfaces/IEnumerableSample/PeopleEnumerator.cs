fe5276aa600dc985ea1ab893a7ba8481e71f8621
Matthew Layton <matthew.layton@live.co.uk>
2018-04-06 10:17:45 +0100

    Added Advanced Topics (Dot Net Interfaces)

---
+++ AdvancedTopics/DotNetInterfaces/IEnumerableSample/PeopleEnumerator.cs
@@ -0,0 +1,21 @@
+﻿namespace IEnumerableSample
+{
+    using System.Collections;
+
+    internal sealed class PeopleEnumerator : IEnumerator
+    {
+        private readonly Person[] people;
+        int position = -1;
+
+        public PeopleEnumerator(Person[] people)
+        {
+            this.people = people;
+        }
+
+        public object Current => people[position];
+
+        public bool MoveNext() => ++position < people.Length;
+
+        public void Reset() => position = -1;
+    }
+}
---
+++ AdvancedTopics/DotNetInterfaces/IEnumerableSample/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace IEnumerableSample
+{
+    using System;
+
+    public sealed class Person : IEquatable<Person>
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public static bool operator ==(Person a, Person b)
+        {
+            return Equals(a, b);
+        }
+
+        public static bool operator !=(Person a, Person b)
+        {
+            return !Equals(a, b);
+        }
+
+        public bool Equals(Person other)
+        {
+            return !(other is null)
+                && FirstName == other.FirstName
+                && LastName == other.LastName;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as Person);
+
+        public override int GetHashCode() => new { FirstName, LastName }.GetHashCode();
+
+        public override string ToString() => $"{FirstName} {LastName}";
+    }
+}
